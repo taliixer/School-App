924bb60638b3161e4ef2f19cf452b778a87d1906
Bryan Vera <brayanandresveragar@gmail.com>
2023-03-30 17:23:08 -0500

    :art: Cree el modal de menu
    Creacion semantica del modal para el menu con un navbar, y estilizados

---
+++ script.js
@@ -0,0 +1,6 @@
+const modal = document.querySelector(".modal__menu")
+const buttonMenu = document.getElementById("menu")
+
+buttonMenu.addEventListener("click",function(){
+    modal.style.top = '0'
+})
